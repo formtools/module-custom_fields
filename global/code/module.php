5ffe2fb4334ec5c00732123969fe5bb77bb68b3f
Ben Keen <ben.keen@gmail.com>
2014-05-30 17:57:27 -0700

    1.0.7

--- global/code/module.php
+++ global/code/module.php
@@ -1,1 +1,6 @@
-<?php function custom_fields__install($x0b){global $x0c, $x0d;$x0e = true;$x0f = "";$x10 = isset($_POST["\x65\x6b"]) ? $_POST["\145\153"] : "";$x11= isset($_POST["\x6b"]) ? $_POST["\153"] : "";if (empty($x10) || empty($x11) || $x10 != crypt($x11, "\147\x77")){$x0e = false;}return array($x0e, $x0f);}
+<?php
+
+function custom_fields__install($module_id)
+{
+	return array(true, "");
+}
--- module.php
+++ module.php
@@ -1,18 +1,18 @@
 <?php
 
 $MODULE = array();
-$MODULE["author"]          = "Encore Web Studios";
-$MODULE["author_email"]    = "formtools@encorewebstudios.com";
-$MODULE["author_link"]     = "http://www.encorewebstudios.com";
-$MODULE["version"]         = "1.0.6";
-$MODULE["date"]            = "2011-11-21";
-$MODULE["is_premium"]      = "yes";
+$MODULE["author"]          = "Ben Keen";
+$MODULE["author_email"]    = "ben.keen@gmail.com";
+$MODULE["author_link"]     = "http://www.formtools.org";
+$MODULE["version"]         = "1.0.7";
+$MODULE["date"]            = "2014-05-30";
+$MODULE["is_premium"]      = "no";
 $MODULE["origin_language"] = "en_us";
 
 
 $MODULE["nav"] = array(
-  "module_name"             => array('{$module_dir}/index.php', false),
-  "phrase_shared_resources" => array('{$module_dir}/shared_resources/', false),
-  "phrase_shared_characteristics" => array('{$module_dir}/shared_characteristics.php', false),
-  "word_license" => array('{$module_dir}/license.php', false)
+	"module_name"             => array('{$module_dir}/index.php', false),
+	"phrase_shared_resources" => array('{$module_dir}/shared_resources/', false),
+	"phrase_shared_characteristics" => array('{$module_dir}/shared_characteristics.php', false),
+	"word_license" => array('{$module_dir}/license.php', false)
 );
